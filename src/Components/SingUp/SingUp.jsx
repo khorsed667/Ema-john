3a92befa4742da2e39383716c146b490998d9c15
khorsed667 <khorsedalam667@gmail.com>
2023-04-18 12:43:51 +0600

    router add also firebase setup completed

---
+++ src/Components/SingUp/SingUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SingUp.css'
+
+const SingUp = () => {
+    return (
+        <div>
+            <p>Sing up function are coming</p>
+        </div>
+    );
+};
+
+export default SingUp;
---
+++ src/Fireebase/Firebase.config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDWuo3FzsddOQza-fxTY-CWu5SRTa0HPeQ",
+  authDomain: "ema-john-firebase-auth-62256.firebaseapp.com",
+  projectId: "ema-john-firebase-auth-62256",
+  storageBucket: "ema-john-firebase-auth-62256.appspot.com",
+  messagingSenderId: "419979230148",
+  appId: "1:419979230148:web:9d31f759bcfcb41f8f4291",
+  measurementId: "G-HSYTFGFET9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export default app;
